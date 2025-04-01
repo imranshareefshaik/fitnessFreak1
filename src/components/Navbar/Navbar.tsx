11544bdaf6d3398e4322dc278b0fdd91aa035542
2000031895imran <2000031895@kluniverity>
2025-04-01 09:19:40 +0530

    day2

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,18 +1,43 @@
+"use client"
 import React from 'react'
 import logo from '@/assets/logo.png'
 import { IoIosBody } from 'react-icons/io'
 import './Navbar.css' // Ensure the file exists!
 import Image from 'next/image'
 import Link from 'next/link'
+import AuthPopup from '../AuthPopup/AuthPopup'
+
+
 
 const Navbar = () => {
+  const [isloggedin, setIsloggedin] = React.useState<boolean>(false)
+
+  const [showpopup, setShowpopup] = React.useState<boolean>(false)
+
+
   return (
       <nav>
         <Image src={logo} alt="Logo" />
         <Link href='/'>Home</Link>
         <Link href='/about'>About</Link>
         <Link href='/profile'><IoIosBody /></Link>
-        <button>Sign Out</button>
+        {
+                isloggedin ?
+                    <button>Logout</button>
+                    :
+                    <button
+                        onClick={() => {
+                            setShowpopup(true)
+                        }}
+                    >Login</button>
+
+
+
+            }
+
+            {
+                showpopup && <AuthPopup setShowpopup={setShowpopup} />
+            }
     </nav>
   )
 }
