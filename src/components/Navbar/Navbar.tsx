b095d50a7b8fddc21192c3e51e1eb0c66110c69f
2000031895imran <2000031895@kluniverity>
2025-04-02 23:13:42 +0530

    day5

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,26 +2,46 @@
 import React from 'react'
 import logo from '@/assets/logo.png'
 import { IoIosBody } from 'react-icons/io'
-import './Navbar.css' // Ensure the file exists!
+import './Navbar.css'
 import Image from 'next/image'
 import Link from 'next/link'
 import AuthPopup from '../AuthPopup/AuthPopup'
-
-
-
 const Navbar = () => {
-  const [isloggedin, setIsloggedin] = React.useState<boolean>(false)
+    const [isloggedin, setIsloggedin] = React.useState<boolean>(false)
 
-  const [showpopup, setShowpopup] = React.useState<boolean>(false)
+    const [showpopup, setShowpopup] = React.useState<boolean>(false)
+    const checklogin = async () => {
+        fetch(process.env.NEXT_PUBLIC_BACKEND_API + '/auth/checklogin', {
+            method: 'POST',
+            credentials: 'include',
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.ok) {
+                    setIsloggedin(true)
+                }
+                else{
+                    setIsloggedin(false)
+                }
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
 
 
-  return (
-      <nav>
-        <Image src={logo} alt="Logo" />
-        <Link href='/'>Home</Link>
-        <Link href='/about'>About</Link>
-        <Link href='/profile'><IoIosBody /></Link>
-        {
+    React.useEffect(() => {
+        checklogin()
+    }, [showpopup])
+
+    return (
+        <nav>
+            <Image src={logo} alt="Logo" />
+            <Link href='/'>Home</Link>
+            <Link href='/about'>About</Link>
+            <Link href='/profile'><IoIosBody /></Link>
+            {
                 isloggedin ?
                     <button>Logout</button>
                     :
@@ -38,8 +58,8 @@ const Navbar = () => {
             {
                 showpopup && <AuthPopup setShowpopup={setShowpopup} />
             }
-    </nav>
-  )
+        </nav>
+    )
 }
 
 export default Navbar
